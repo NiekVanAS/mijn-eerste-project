c12291621f99c9312b3120f76e163dd1dfab83ce
Niek van Aalst <nvanaalst441@student.novacollege.nl>
2024-11-28 14:30:17 +0100

    quick update

---
+++ www/nav.php
@@ -0,0 +1,11 @@
+<header>
+        <div class="header-top">
+            <h1>Plushy shop</h1>
+        </div>
+        <nav>
+            <div class="nav-bar">
+                <a href="index.php">Home</a>
+                <a href="test.html">test page</a>
+            </div>
+        </nav>
+    </header>
